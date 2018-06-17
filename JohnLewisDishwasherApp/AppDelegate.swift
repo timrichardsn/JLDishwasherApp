f8df2362754450a70a539f6239173c2d229935bf
Tim Richardson <tim@timrichardson.co>
2018-06-17 10:21:06 +0100

    Initial commit

---
+++ JohnLewisDishwasherApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  JohnLewisDishwasherApp
+//
+//  Created by Tim Richardson on 17/06/2018.
+//  Copyright © 2018 Tim Richardson. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
