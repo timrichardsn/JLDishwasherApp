8c3170aef445774e18a13de4ac0c1c3f448162cb
Tim Richardson <tim@timrichardson.co>
2018-06-17 10:27:43 +0100

    Added test for Product Grid router to create and check the Product Grid view

---
+++ JohnLewisDishwasherAppTests/ProductGridTests.swift
@@ -0,0 +1,21 @@
+//
+//  ProductGridTests.swift
+//  JohnLewisDishwasherAppTests
+//
+//  Created by Tim Richardson on 17/06/2018.
+//  Copyright © 2018 Tim Richardson. All rights reserved.
+//
+
+import XCTest
+@testable import JohnLewisDishwasherApp
+
+class ProductGridTests: XCTestCase {
+    
+    func testCreateProductGridView() {
+        
+        let productGridView = ProductGridRouter.createProductGridModule()
+        
+        XCTAssertTrue(productGridView is ProductGridView)
+    }
+
+}
