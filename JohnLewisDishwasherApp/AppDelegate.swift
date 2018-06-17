c3a087026d7d507e6f590d76eced67bfec1056d3
Tim Richardson <tim@timrichardson.co>
2018-06-17 17:17:15 +0100

    Added AppDelegate logic to display the first view controller "ProductGridView"

--- JohnLewisDishwasherApp/AppDelegate.swift
+++ JohnLewisDishwasherApp/AppDelegate.swift
@@ -14,7 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let productGridView = ProductGridRouter.createProductGridModule()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = productGridView
+        window?.makeKeyAndVisible()
+        
         return true
     }
 }
