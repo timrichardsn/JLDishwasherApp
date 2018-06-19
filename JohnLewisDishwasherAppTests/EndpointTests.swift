10b7d892bd594ef21e79f914ed3b4df6543b21f4
Tim Richardson <tim@timrichardson.co>
2018-06-19 20:54:05 +0100

    Moved existing endpoint tests out into new endpoint tests case file

--- JohnLewisDishwasherAppTests/EndpointTests.swift
+++ JohnLewisDishwasherAppTests/EndpointTests.swift
@@ -11,6 +11,31 @@ import XCTest
 
 class EndpointTests: XCTestCase {
     
+    func testRequestData() {
+        
+        let parameters: [RequestParameterKey: Any] = [
+            .product : "dishwasher",
+            .pageSize: "20"
+        ]
+        
+        let requestData = RequestData(endPoint: .products(action: .search), parameters: parameters)
+        
+        XCTAssertEqual(requestData.endPoint.endpointString, "products/search")
+        XCTAssertEqual(requestData.endPoint.urlString, "\(API.rootUrlString)/products/search")
+        XCTAssertNotNil(requestData.parameters)
+        
+        let parametersForRequest = requestData.parametersForRequest(includeKey: true)
+        
+        XCTAssertEqual(parametersForRequest.count, 3)
+        XCTAssertEqual(parametersForRequest[RequestParameterKey.product.stringValue] as? String, parameters[.product] as? String)
+        XCTAssertEqual(parametersForRequest[RequestParameterKey.pageSize.stringValue] as? String, parameters[.pageSize] as? String)
+        XCTAssertEqual(parametersForRequest[RequestParameterKey.key.stringValue] as? String, API.key)
+    }
     
-    
+    func testRequestParameters() {
+        
+        XCTAssertEqual(RequestParameterKey.product.stringValue, "q")
+        XCTAssertEqual(RequestParameterKey.key.stringValue, "key")
+        XCTAssertEqual(RequestParameterKey.pageSize.stringValue, "pageSize")
+    }
 }
