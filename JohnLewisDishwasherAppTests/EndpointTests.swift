933fce6779e3190d60aff69415a374595de2b940
Tim Richardson <tim@timrichardson.co>
2018-06-19 20:52:33 +0100

    Added new test class EndpointTests

---
+++ JohnLewisDishwasherAppTests/EndpointTests.swift
@@ -0,0 +1,16 @@
+//
+//  EndpointTests.swift
+//  JohnLewisDishwasherAppTests
+//
+//  Created by Tim Richardson on 19/06/2018.
+//  Copyright © 2018 Tim Richardson. All rights reserved.
+//
+
+import XCTest
+@testable import JohnLewisDishwasherApp
+
+class EndpointTests: XCTestCase {
+    
+    
+    
+}
