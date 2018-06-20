410b45ed8221861844ff3228c2dcb55d41399043
Tim Richardson <tim@timrichardson.co>
2018-06-20 12:00:00 +0100

    Added price data cell to display price data when the detail view is in portrait mode

---
+++ JohnLewisDishwasherApp/ProductDetail/View/PriceDataCell.swift
@@ -0,0 +1,18 @@
+//
+//  PriceDataCell.swift
+//  JohnLewisDishwasherApp
+//
+//  Created by Tim Richardson on 20/06/2018.
+//  Copyright © 2018 Tim Richardson. All rights reserved.
+//
+
+import UIKit
+
+class PriceDataCell: UITableViewCell {
+
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var specialOfferLabel: UILabel!
+    @IBOutlet weak var guaranteeLabel: UILabel!
+    
+    
+}
