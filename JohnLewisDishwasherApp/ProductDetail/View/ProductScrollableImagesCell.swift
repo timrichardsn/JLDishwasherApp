dcc76ecd9c3d2978bf82a0dbd53ec5dc24e37e4e
Tim Richardson <tim@timrichardson.co>
2018-06-20 10:42:56 +0100

    Started adding UI components for the scrollable images

---
+++ JohnLewisDishwasherApp/ProductDetail/View/ProductScrollableImagesCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductScrollableImagesCell.swift
+//  JohnLewisDishwasherApp
+//
+//  Created by Tim Richardson on 20/06/2018.
+//  Copyright © 2018 Tim Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ProductScrollableImagesCell: UITableViewCell {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+}
